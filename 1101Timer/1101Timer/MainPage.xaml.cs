eeecd0618b633ac332c59df5f2e7e4a64e0c0e36
miga-666 <s108213029@mail1.ncnu.edu.tw>
2021-12-06 15:12:16 +0800

    This is a timer that counts per second

---
+++ 1101Timer/1101Timer/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _1101Timer
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+            int count = 0;
+            Device.StartTimer(new TimeSpan(0, 0, 1), () =>
+            {
+
+                // do something every 60 seconds
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    count++;
+                    printCount.Text = count.ToString();
+                    Console.WriteLine(count);
+                    // interact with UI elements
+                });
+                return true; // runs again, or false to stop
+            });
+        }
+    }
+}
